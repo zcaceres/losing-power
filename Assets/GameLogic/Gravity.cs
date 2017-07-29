ba65457f0c3660bcfbd6eaaafd77708d477bae37
zc <zachcaceres@gmail.com>
2017-07-28 22:47:51 -0400

    gravity sort of

---
+++ Assets/GameLogic/Gravity.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour {
+	private SphereCollider gravitationalField;
+
+	void Awake() {
+		gravitationalField = GetComponent<SphereCollider>();
+		Debug.Log(gravitationalField);
+	}
+
+	void Start () {
+
+	}
+
+
+
+	void Update () {
+
+	}
+}
