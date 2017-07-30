fd995ab9721ad76411e4aa1f12ea626ac526a650
zc <zachcaceres@gmail.com>
2017-07-30 16:07:51 -0400

    remove debug logs

--- Assets/GameLogic/Light/LightTrigger.cs
+++ Assets/GameLogic/Light/LightTrigger.cs
@@ -6,20 +6,12 @@ public class LightTrigger : MonoBehaviour {
 	protected virtual void OnTriggerEnter (Collider other) {
 		if (other.tag == "Enemy") {
 			other.GetComponent<EnemyController>().GotLit(true);
-			Debug.Log("Deactivating " + other);
 		}
 	}
-
-	// protected virtual void OnTriggerStay (Collider other) {
-	// 	if (other.tag == "") {
-	//
-	// 	}
-	// }
 
 	protected virtual void OnTriggerExit (Collider other) {
 		if (other.tag == "Enemy") {
 			other.GetComponent<EnemyController>().GotLit(false);
-			Debug.Log("Activating " + other);
 		}
 	}
 }
